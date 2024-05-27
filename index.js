689349d3a14bb6d922323569e6beef441b898945
Maycon Arruda <maycon.unix@gmail.com>
2024-05-27 10:29:23 -0300

    reomove cors problems hahahaahha

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 // include dependencies
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // create the proxy
 /** @type {import('http-proxy-middleware/dist/types').RequestHandler<express.Request, express.Response>} */
 const exampleProxy = createProxyMiddleware({
-  target: 'https://receitaws.com.br/', // target host with the same base path
+  target: "https://receitaws.com.br/", // target host with the same base path
   changeOrigin: true, // needed for virtual hosted sites
 });
 
 // mount `exampleProxy` in web server
-app.use('/', exampleProxy);
+app.use("/", exampleProxy);
 app.listen(3000);
